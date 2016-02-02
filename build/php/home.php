139c13f861a0ea563532f1fd3a7b6a00d2255450
Willowl <kvashaw@gmail.com>
2016-02-02 00:08:21 -0500

    home.php edits

--- build/php/home.php
+++ build/php/home.php
@@ -1,5 +1,13 @@
 <?php require_once 'template_top.php' ?>
 <!-- Beginning of Body -->
-<p class='row collapse'>Hello World!</p>
+<div class='row collapse'>
+  <div class='large-6 columns'>
+    <p>The first six columns</p>
+  </div>
+  <div class='large-6 columns'>
+    <p>The second six columns</p>
+  </div>
+</div>
+
 <!-- End of Body -->
 <?php require_once 'template_bottom.php' ?>
