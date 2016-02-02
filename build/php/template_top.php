58369af1babe013f580088969f00b789b8f368b1
Willowl <kvashaw@gmail.com>
2016-02-02 13:08:25 -0500

    fixed filename

--- build/php/template_top.php
+++ build/php/template_top.php
@@ -11,9 +11,8 @@
     <meta charset = "UTF-8" name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="restaurant, allergy, allergen, food, options">
     <meta name="content" content="A database for various allergies and restaurants in Boone">
-    <link rel="stylesheet" type="text/css" href="../css/index.css">
+    <link rel="stylesheet" type="text/css" href="../css/styles.css">
     <link rel="shortcut icon" href="../pictures/favicon.ico">
-    <link href='http://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" type="text/css" href="../../node_modules/foundation/scss/foundation.css">
     <script src='../../node_modules/jquery/dist/jquery.js'></script>
   </head>
