063b6eddf021f72470b2c7075214b5445fbff124
Kira Willow Vashaw <kvashaw@gmail.com>
2016-04-04 06:30:27 -0400

    fixed gitignore

--- mynotes.js
+++
@@ -1,9 +0,0 @@
-
-$http.get("api.php/user?filter[]=email,eq," + $scope.user.email).then(function(response) {
-  $scope.myData = response.data;
-  $scope.users = response.data.user.records;
-});
-$http.get('/api.php/user?filter[]=email,eq,' + $scope.user.email).then(function(response) {
-  $scope.myData  = response.data;
-  $scope.myData2 = response.data.user.records[0][0];
-});
