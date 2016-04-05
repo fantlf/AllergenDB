6cfe0d6a22e3e4443235fdcc0b0f4a9fa65d8544
Kira Willow Vashaw <kvashaw@gmail.com>
2016-04-05 14:19:07 -0400

    cleaned up account system

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -58467,19 +58467,18 @@ function HomeCtrl($rootScope, $scope) {
 ;HCDietsApp.controller('LoginCtrl', LoginCtrl);
 
 function LoginCtrl($location, $scope, AuthenticationService) {
-    $scope.login = login;
-    AuthenticationService.clearCredentials();
+  $scope.login = login;
+  AuthenticationService.clearCredentials();
 
-    function login() {
-      AuthenticationService.login(user.email, user.pass).then(function(response) {
-        if (response)
-          $location.path('/');
-        else
-          $scope.error = response.error;
-      });
-    }
-
+  function login() {
+    AuthenticationService.login(user.email, user.pass).then(function(response) {
+      if (response)
+        $location.path('/');
+      else
+        $scope.error = response.error;
+    });
   }
+}
 ;HCDietsApp.controller('LogoutCtrl', LogoutCtrl);
 
 function LogoutCtrl($location, $http, $rootScope, $cookies) {
--- src/js/components/login/login.controller.js
+++ src/js/components/login/login.controller.js
@@ -1,16 +1,15 @@
 HCDietsApp.controller('LoginCtrl', LoginCtrl);
 
 function LoginCtrl($location, $scope, AuthenticationService) {
-    $scope.login = login;
-    AuthenticationService.clearCredentials();
+  $scope.login = login;
+  AuthenticationService.clearCredentials();
 
-    function login() {
-      AuthenticationService.login(user.email, user.pass).then(function(response) {
-        if (response)
-          $location.path('/');
-        else
-          $scope.error = response.error;
-      });
-    }
-
+  function login() {
+    AuthenticationService.login(user.email, user.pass).then(function(response) {
+      if (response)
+        $location.path('/');
+      else
+        $scope.error = response.error;
+    });
   }
+}
