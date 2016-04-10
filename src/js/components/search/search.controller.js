90142aac202148f88ce2eb116013b0ff123c41e3
Kira Willow Vashaw <kvashaw@gmail.com>
2016-04-10 16:29:23 -0400

    ...

--- src/js/components/search/search.controller.js
+++ src/js/components/search/search.controller.js
@@ -9,11 +9,6 @@ function SearchCtrl($scope, $http) {
     $http.get("/3430/161/team7/AllergenDB/public/php/search.php?query=" + $scope.query)
     .then(function (response) {
       $scope.searchResults = response.data.records;
-      /*$scope.searchResults = {keys : [], vals : []};
-      Object.keys(response.data.records).forEach(function(key,index) {
-        $scope.searchResults.keys.push(key);
-        $scope.searchResults.vals.push(index);
-      });*/
     });
   }
 
