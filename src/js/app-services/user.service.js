56f51227634029bab6da79f2ce7ccca09cee98ec
Kira Willow Vashaw <kvashaw@gmail.com>
2016-04-13 22:13:39 -0400

    fixed filepath

--- src/js/app-services/user.service.js
+++ src/js/app-services/user.service.js
@@ -14,37 +14,37 @@ function UserService($http) {
     return service;
 
     function GetById(id) {
-      return $http.get('/3430/161/team7/api.php/user?filter[]=id,eq,' + id).then(handleSuccess, handleError('Error getting user by id'));
+      return $http.get('/3430/161/team7/HighCountryDiets/api.php/user?filter[]=id,eq,' + id).then(handleSuccess, handleError('Error getting user by id'));
     }
 
     function GetPass(id) {
-      return $http.get('/3430/161/team7/api.php/pass?filter[]=userid,eq,' + id).then(handleSuccess, handleError('Error getting password'));
+      return $http.get('/3430/161/team7/HighCountryDiets/api.php/pass?filter[]=userid,eq,' + id).then(handleSuccess, handleError('Error getting password'));
     }
 
     function GetByEmail(emailvalue) {
-      return $http.get('/3430/161/team7/api.php/user?filter[]=email,eq,' + emailvalue).then(handleSuccess, handleError('Error getting user by email'));
+      return $http.get('/3430/161/team7/HighCountryDiets/api.php/user?filter[]=email,eq,' + emailvalue).then(handleSuccess, handleError('Error getting user by email'));
     }
 
     function GetByUname(unamevalue) {
-      return $http.get('/3430/161/team7/api.php/user?filter[]=uname,eq,' + unamevalue).then(handleSuccess, handleError('Error getting user by email'));
+      return $http.get('/3430/161/team7/HighCountryDiets/api.php/user?filter[]=uname,eq,' + unamevalue).then(handleSuccess, handleError('Error getting user by email'));
     }
 
     function Create(user) {
-      return $http.post('/3430/161/team7/api.php/user', { email : user.email, fname : user.fname, sname : user.sname, uname : user.uname} ).then(function() {
-        $http.get('/3430/161/team7/api.php/user?filter[]=email,eq,' + user.email).then(function(response) {
+      return $http.post('/3430/161/team7/HighCountryDiets/api.php/user', { email : user.email, fname : user.fname, sname : user.sname, uname : user.uname} ).then(function() {
+        $http.get('/3430/161/team7/HighCountryDiets/api.php/user?filter[]=email,eq,' + user.email).then(function(response) {
           var id = response.data.user.records[0][0];
           var code = CryptoJS.SHA256(user.pass1).toString();
-          $http.post('/3430/161/team7/api.php/pass', { userid : id, pass: code } ).then(handleSuccess, handleError("Failed to create user"));
+          $http.post('/3430/161/team7/HighCountryDiets/api.php/pass', { userid : id, pass: code } ).then(handleSuccess, handleError("Failed to create user"));
         }, handleError("Failed to create user"));
       }, handleError("Failed to create user"));
     }
 
     function Update(user) {
-      return $http.put('/3430/161/team7/api.php/user/' + user.id, user).then(handleSuccess, handleError('Error updating user'));
+      return $http.put('/3430/161/team7/HighCountryDiets/api.php/user/' + user.id, user).then(handleSuccess, handleError('Error updating user'));
     }
 
     function Delete(id) {
-      return $http.delete('/3430/161/team7/api.php/user/' + id).then(handleSuccess, handleError('Error deleting user'));
+      return $http.delete('/3430/161/team7/HighCountryDiets/api.php/user/' + id).then(handleSuccess, handleError('Error deleting user'));
     }
 
     // private functions
