800b4af75b8eec310593be513eaad55baac8e727
Kira Willow Vashaw <kvashaw@gmail.com>
2016-04-24 16:32:09 -0400

    setup recipe form page

--- src/js/components/recipeform/recipeform.controller.js
+++ src/js/components/recipeform/recipeform.controller.js
@@ -1,6 +1,47 @@
 HCDietsApp.controller('RecipeformCtrl', RecipeformCtrl);
 
-RecipeformCtrl.$inject = ['$rootScope', '$scope'];
-function RecipeformCtrl($rootScope, $scope) {
+RecipeformCtrl.$inject = ['SearchService', '$rootScope', '$scope'];
+function RecipeformCtrl(SearchService, $rootScope, $scope) {
+  $scope.dreqSelect = dreqSelect;
+  $scope.addRecipe = addRecipe;
+  $scope.dietaryreqs = [];
+  $scope.ingredients = [];
+  $scope.
+  SearchService.getDietaryreqs().then(function(response) {
+    var results = response.data.dietaryreq.records;
+    for (var i = 0; i < results.length; i++) {
+      $scope.dietaryreqs[i] = {
+        id : results[i][0],
+        name : results[i][1],
+        class : "notSelected",
+        input : false
+      };
+    }
+  });
 
+  function addRecipe() {
+
+  }
+
+  function buildQuery() {
+
+  }
+
+  function addIngredient() {
+    ingredients.push({
+      name : $scope.newIngredientName;
+      description : $scope.newIngredientDesc;
+    });
+  }
+
+  function dreqSelect(index) {
+    if ($scope.dietaryreqs[index].input) {
+      $scope.dietaryreqs[index].input = false;
+      $scope.dietaryreqs[index].class = "notSelected";
+    }
+    else {
+      $scope.dietaryreqs[index].input = true;
+      $scope.dietaryreqs[index].class = "selected";
+    }
+  }
 }
