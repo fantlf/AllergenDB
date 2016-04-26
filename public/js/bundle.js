7dff5bb3a26ec0dfe8eb93481727260f668b2f20
Kira Willow Vashaw <kvashaw@gmail.com>
2016-04-26 03:51:56 -0400

    fixed recipe display

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -59061,14 +59061,14 @@ function ProfileCtrl($location, UserService, $rootScope, $scope) {
 
 RecipeCtrl.$inject = ['SearchService', '$rootScope', '$scope'];
 function RecipeCtrl(SearchService, $rootScope, $scope) {
-  $scope.recipe = {id : "", name : "", description : "", directions : ""};
+  $scope.recipe = {id : "", name : "", description : "", steps : []};
   $scope.ingredients = [];
   $scope.addComment = addComment;
   SearchService.getRecipeById($rootScope.currRecipe).then(function(response) {
     var results = response.data.recipe.records[0];
     $scope.recipe.id = results[0];
     $scope.recipe.name = results[1];
-    $scope.recipe.description = results[2];
+    $scope.recipe.steps = breakSteps(results[2]);
     $scope.recipe.directions = results[3];
     var query = "SELECT id, name, quantity FROM ingredient, reqingredient WHERE id=ingredientid AND recipeid=" + $scope.recipe.id;
     SearchService.runSearchQuery(query).then(function(response) {
@@ -59083,6 +59083,12 @@ function RecipeCtrl(SearchService, $rootScope, $scope) {
 
   }
 
+  //Private Functions
+
+  function breakSteps(steps) {
+    newSteps = steps.split("~~~");
+    return newSteps;
+  }
 
 }
 ;HCDietsApp.controller('RecipeformCtrl', RecipeformCtrl);
