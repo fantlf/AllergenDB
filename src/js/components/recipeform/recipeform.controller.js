bf657005122061d8f7460f59e9cc104491486de6
Willowl <kvashaw@gmail.com>
2016-04-27 12:29:32 -0400

    fixecd

--- src/js/components/recipeform/recipeform.controller.js
+++ src/js/components/recipeform/recipeform.controller.js
@@ -59,7 +59,7 @@ function RecipeformCtrl(RecipeService, SearchService, $location, $rootScope, $sc
     $scope.results = query;
     RecipeService.runInsertQuery(query).then(function(response) {
       if (response.data.records[0].result != "Error") {
-        //$location.path('/profile');
+        $location.path('/profile');
         $scope.result = query;
       } else {
         alert("Oops! Something went wrong. We're working to fix it, try again later.");
