011fe44d57c325d9826ef99df7c12ab4014572a4
Willowl <kvashaw@gmail.com>
2016-04-27 12:37:50 -0400

    test

--- src/query/index.php
+++ src/query/index.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 $query = $_GET['query'];
+echo $query;
 $insertTest = substr($query, 0, 5);
 $outp = "";
 
